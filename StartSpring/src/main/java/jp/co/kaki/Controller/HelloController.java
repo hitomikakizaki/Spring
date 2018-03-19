1f07f9067a75f2b0c586023d71f0b606da0e1511
kaki <hitomi_kakizaki@densan-s.co.jp>
2018-03-19 18:35:42 +0900

    jsp変更

--- StartSpring/src/main/java/jp/co/kaki/Controller/HelloController.java
+++ StartSpring/src/main/java/jp/co/kaki/Controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home() {
-		return "startSpring";
+		return "startSpring.jsp";
 	}
 }
